aa75d0c380a5c883ec9217089f0fe9b508a81d62
vathsa1708 <111900052+vathsa1708@users.noreply.github.com>
2023-06-18 12:31:21 +0530

    Create BoundaryTraversal.cpp

---
+++ BoundaryTraversal.cpp
@@ -0,0 +1,193 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+
+
+
+
+
+
+
+// } Driver Code Ends
+/* A binary tree Node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+class Solution {
+public:
+vector<int>ans;
+vector<int>t;
+bool leaf(Node *root)
+{
+    return (!root->left and !root->right);
+}
+void leftview(Node *root)
+{
+    if(!root)
+    return;
+   
+    Node *ptr = root->left;
+    while(ptr!=NULL)
+    {
+        if(!leaf(ptr))
+        ans.push_back(ptr->data);
+        if(ptr->left!=NULL)
+        ptr = ptr->left;
+        else
+        ptr = ptr->right;
+    }
+}
+void rightview(Node *root)
+{
+    if(!root)
+    return;
+    Node *ptr = root->right;
+    while(ptr!=NULL)
+    {
+        if(!leaf(ptr))
+        t.push_back(ptr->data);
+        if(ptr->right!=NULL)
+        ptr = ptr->right;
+        else
+        ptr = ptr->left;
+    }
+    for(int i = t.size()-1;i>=0;i--)
+    {
+        ans.push_back(t[i]);
+    }
+}
+void leafview(Node *root)
+{
+    if(!root)
+    return;
+    if(leaf(root))
+    ans.push_back(root->data);
+    leafview(root->left);
+    leafview(root->right);
+}
+
+
+    vector <int> boundary(Node *root)
+    {
+        //Your code here
+        if(!leaf(root))
+         ans.push_back(root->data);
+        leftview(root);
+        leafview(root);
+        rightview(root);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s ,ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        vector <int> res = ob.boundary(root);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
